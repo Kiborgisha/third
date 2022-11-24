ae09b67c44d5475f9d4b3e18ecb98cbad96447d2
Kiborgisha <115732998+Kiborgisha@users.noreply.github.com>
2022-11-24 20:48:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelector("button").onclick = function(){
+    let input1 = document.querySelector("input").value
+    let input2 = document.querySelector(".sname").value
+    alert(input1 + " " + input2 + " ваше сообщение отправлено")
+}
